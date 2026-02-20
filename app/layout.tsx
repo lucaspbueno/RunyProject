52771c4d52d6620fb5b90dc1d10d8de0c129ae0c
lucaspbueno <lucaspbueno22@gmail.com>
2026-02-20 01:52:01 -0300

    style: atualização de estilos globais e configurações de fontes
    - Adicionadas famílias de fontes personalizadas para os elementos `body` e `code/pre` no arquivo `globals.css` para aprimorar a tipografia.
    - Configurações de fontes atualizadas no arquivo `layout.tsx` para incluir opções de fallback e configurações de exibição para as fontes Geist.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,15 @@ import { ThemeProvider } from "@/components/theme-provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  fallback: ["system-ui", "arial", "sans-serif"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  fallback: ["ui-monospace", "SFMono-Regular", "Monaco", "Consolas", "monospace"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
